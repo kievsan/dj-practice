49938df4924f4475cd7ca61bd4899c8e517c9bc2
Sergey Kievskiy <knhel7@mail.ru>
2023-04-07 14:52:13 +0300

    vews.py and urls.py was edited. Pages redirect and data output done

--- 1.1-first-project/first_project/app/views.py
+++ 1.1-first-project/first_project/app/views.py
@@ -1,3 +1,7 @@
+import datetime
+import os
+from pprint import pprint
+
 from django.http import HttpResponse
 from django.shortcuts import render, reverse
 
@@ -8,8 +12,8 @@ def home_view(request):
     # функцию `reverse`
     pages = {
         'Главная страница': reverse('home'),
-        'Показать текущее время': '',
-        'Показать содержимое рабочей директории': ''
+        'Показать текущее время': reverse('time'),
+        'Показать содержимое рабочей директории': reverse('workdir')
     }
     
     # context и параметры render менять не нужно
@@ -23,7 +27,7 @@ def home_view(request):
 def time_view(request):
     # обратите внимание – здесь HTML шаблона нет, 
     # возвращается просто текст
-    current_time = None
+    current_time = datetime.datetime.now().time()
     msg = f'Текущее время: {current_time}'
     return HttpResponse(msg)
 
@@ -32,4 +36,7 @@ def workdir_view(request):
     # по аналогии с `time_view`, напишите код,
     # который возвращает список файлов в рабочей 
     # директории
-    raise NotImplemented
+    template_name = 'app/workdir.html'
+    work_list = os.listdir(os.getcwd())
+    pprint(work_list)
+    return render(request, template_name, context={'work_list': work_list})
